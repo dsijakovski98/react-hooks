0c06e57e75478e3c80c09e5e83609f472f5dd9e4
dshijakovskiTDL <daniel.shijakovski@testdevlab.com>
2022-07-31 13:48:11 +0200

    initial test updated

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,5 +1,8 @@
+import { render } from '@testing-library/react'
+import App from '../src/App'
+
 describe('Initial test case for App component', () => {
     it('Renders successfully', () => {
-        expect(true).toBe(true)
+        render(<App />)
     })
 })
